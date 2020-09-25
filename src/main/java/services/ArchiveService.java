7af340cfbc0c842ca2f9334a368e77933180e9b9
andrejtarasov <tarasov_av@cs.vsu.ru>
2020-09-25 23:41:37 +0300

    add methods to archive service, may be it better to be deleted

--- src/main/java/services/ArchiveService.java
+++ src/main/java/services/ArchiveService.java
@@ -1,5 +1,6 @@
 package services;
 
+import entitys.Archive;
 import entitys.File;
 import interfaces.DAO;
 
@@ -8,42 +9,62 @@ import java.util.List;
 
 public class ArchiveService {
 
-    private DAO archive;
+    private DAO dao;
 
-    public ArchiveService(DAO archive) {
-        this.archive = archive;
+    public void choseArchive(int archiveId){
+        dao.choseArchive(archiveId);
+    }
+
+    public void addArchive(String name){
+        dao.addArchive(name);
+    }
+
+    public void deleteArchive(int id){
+        dao.deleteArchive(id);
+    }
+
+    public List<Archive> getAllArchives(){
+        return dao.getAllArchives();
+    }
+
+    public List<File> getAllFiles(){
+        return dao.getAllFiles();
+    }
+
+    public ArchiveService(DAO dao) {
+        this.dao = dao;
     }
 
     public void addFile(File file){
-        archive.addFile(file);
+        dao.addFile(file);
     }
 
     public File getFile(int id){
-        return archive.getFile(id);
+        return dao.getFile(id);
     }
 
     public void removeFile(int id){
-        archive.removeFile(id);
+        dao.removeFile(id);
     }
 
     public void editFile(int id, File file){
-        archive.editFile(id, file);
+        dao.editFile(id, file);
     }
 
     public List<File> getByType(String key){
-        return archive.getByType(key);
+        return dao.getByType(key);
     }
 
     public List<File> getByName(String key){
-        return archive.getByName(key);
+        return dao.getByName(key);
     }
 
     public List<File> getByDate(Date key){
-        return archive.getByDate(key);
+        return dao.getByDate(key);
     }
 
     public List<File> getByArguments(String name, String type, Date from, Date to){
-        return archive.getByArguments(name, type, from, to);
+        return dao.getByArguments(name, type, from, to);
     }
 
 }
