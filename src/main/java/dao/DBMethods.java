379431376e38d32d89356c466a2bf24778ca7ec0
andrejtarasov <tarasov_av@cs.vsu.ru>
2020-10-28 21:36:13 +0300

    done SQLQueryMaker

--- src/main/java/dao/DBMethods.java
+++ src/main/java/dao/DBMethods.java
@@ -1,14 +1,10 @@
 package dao;
 
-import lombok.Getter;
 
 public enum DBMethods {
-    // TODO: 27/10/2020 переделать из enum d class
     INSERT,
     SELECT,
     UPDATE,
     DELETE,
-    CREATE;
-
-
+    CREATE
 }
--- src/main/java/dao/SQLQueryMaker.java
+++ src/main/java/dao/SQLQueryMaker.java
@@ -1,14 +1,18 @@
 package dao;
 
+import annotations.db.Column;
 import annotations.db.Table;
-import dao.SQLMethods.Create;
-import dao.SQLMethods.Query;
+import dao.SQLMethods.*;
+import lombok.NonNull;
+import lombok.SneakyThrows;
 
 import java.lang.annotation.AnnotationTypeMismatchException;
+import java.lang.reflect.Field;
 
 public class SQLQueryMaker implements QueryMaker {
 
-    public<T> Query makeQuery(DBMethods method, T entity, String... conditions) {
+    @SneakyThrows
+    public<T> Query makeQuery(@NonNull DBMethods method, T entity, String... conditions) {
         if (!entity.getClass().isAnnotationPresent(Table.class))
             throw new AnnotationTypeMismatchException(null, "class is not annotated");
 
@@ -16,30 +20,34 @@ public class SQLQueryMaker implements QueryMaker {
         if (table.equals(""))
             table = entity.getClass().getName();
 
-        switch (method) {
-            case CREATE:
-                return new Create(table,new String[]{""}); // TODO: 28/10/2020 переделать create metod 
-            case DELETE:
-
-            case SELECT:
-
-            case UPDATE:
-
-            case INSERT:
-
+        StringBuilder conBuilder = new StringBuilder();
+        for (String condition : conditions) {
+            conBuilder.append(condition).append(" AND ");
         }
 
+        Field[] fields = entity.getClass().getFields();
+        String[] columnNames = new String[fields.length];
+        String[] values = new String[fields.length];
 
-    }
+        for (int i = 0; i < fields.length; i++) {
+            columnNames[i] = fields[i].getAnnotation(Column.class).name();
+            values[i] = fields[i].get(entity).toString();
+        }
 
-    private<T> String queryMaker(DBMethods method, T entity, String postfixes){
-        if (!entity.getClass().isAnnotationPresent(Table.class))
-            throw new AnnotationTypeMismatchException(null, "class is not annotated");
-        String table = entity.getClass().getAnnotation(Table.class).name();
-        if (table.equals("")) table = entity.getClass().getName();
+        switch (method) {
+            case CREATE:
+                return new Create(table,new String[]{""}); // TODO: 28/10/2020 переделать create method
+            case DELETE:
+                return new Delete(table, conBuilder.toString());
+            case SELECT:
+                return new Select(table, conBuilder.toString());
+            case UPDATE:
+                return new Update(table, columnNames, values, conBuilder.toString());
+            case INSERT:
+                return new Insert(table,columnNames,values);
+        }
 
-//        StringBuilder sb = new StringBuilder();
-//        sb.append(String.format())
-        return "";
+        throw new Exception("need some realisation for new data base method");
+
     }
 }
