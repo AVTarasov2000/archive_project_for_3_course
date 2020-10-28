8521caeb5db13d2739bebd3a6c55df9c458fe9c4
AVTarasov <andrey89066805728@ya.ru>
2020-10-28 15:50:41 +0300

    logic in QueryMaker

--- src/main/java/dao/SQLQueryMaker.java
+++ src/main/java/dao/SQLQueryMaker.java
@@ -1,5 +1,9 @@
 package dao;
 
+import annotations.db.Table;
+
+import java.lang.annotation.AnnotationTypeMismatchException;
+
 public class SQLQueryMaker implements QueryMaker {
 
     public<T> Query makeQuery(DBMethods method, T entity, String... postfixes) {
@@ -10,5 +14,16 @@ public class SQLQueryMaker implements QueryMaker {
         }
         // TODO: 24/10/2020 убрать new
         return new SQLQuery(String.format(method.getPrefix(), entity, sb.toString()));
+    }
+
+    private<T> String queryMaker(DBMethods method, T entity, String postfixes){
+        if (!entity.getClass().isAnnotationPresent(Table.class))
+            throw new AnnotationTypeMismatchException(null, "class is not annotated");
+        String table = entity.getClass().getAnnotation(Table.class).name();
+        if (table.equals("")) table = entity.getClass().getName();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format())
+        return "";
     }
 }
